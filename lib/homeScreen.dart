9817977bcdc9739d7426df8f35943e5e1c223d83
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-10-19 21:00:50 +0530

    done

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:cryptography/cryptography.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,6 +13,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   PlatformFile file;
   String filetext;
+  bool fileUploaded = false;
   Future<String> _read(String path) async {
     String text;
     try {
@@ -24,8 +25,10 @@ class _HomeScreenState extends State<HomeScreen> {
     return text;
   }
 
+  List encrypted;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -36,44 +39,125 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () async {
-            FilePickerResult result = await FilePicker.platform.pickFiles();
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: size.height * 3 / 100,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: size.width * 5 / 100),
+            child: Text(
+              'Encode',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: size.height * 1 / 100,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.all(
+                Radius.circular(size.width * 2 / 100),
+              ),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: size.width * 5 / 100),
+            child: FlatButton(
+              onPressed: () async {
+                FilePickerResult result = await FilePicker.platform.pickFiles();
 
-            if (result != null) {
-              PlatformFile file = result.files.first;
-              filetext = await _read(file.path);
-            }
-            final cipher = chacha20Poly1305Aead;
+                if (result != null) {
+                  PlatformFile file = result.files.first;
+                  filetext = await _read(file.path);
+                  if (filetext != null) {
+                    setState(() {
+                      fileUploaded = true;
+                    });
+                  }
+                }
+                final cipher = chacha20Poly1305Aead;
 
-            final secretKey = SecretKey.randomBytes(32);
+                final secretKey = SecretKey.randomBytes(32);
 
-            final nonce = Nonce.randomBytes(12);
+                final nonce = Nonce.randomBytes(12);
 
-            // Our message
-            final message = utf8.encode(filetext);
+                // Our message
+                final message = utf8.encode(filetext);
 
-            // Encrypt
-            final encrypted = await cipher.encrypt(
-              message,
-              secretKey: secretKey,
-              nonce: nonce,
-            );
+                // Encrypt
+                encrypted = await cipher.encrypt(
+                  message,
+                  secretKey: secretKey,
+                  nonce: nonce,
+                );
 
-            print('Encrypted: $encrypted');
+                print('Encrypted: $encrypted');
 
-            // Decrypt
-            final decrypted = await cipher.decrypt(
-              encrypted,
-              secretKey: secretKey,
-              nonce: nonce,
-            );
-            print(utf8.decode(decrypted));
-            print('Decrypted: $decrypted');
-          },
-          child: Text("Upload File"),
-        ),
+                // Decrypt
+                final decrypted = await cipher.decrypt(
+                  encrypted,
+                  secretKey: secretKey,
+                  nonce: nonce,
+                );
+                print(utf8.decode(decrypted));
+                print('Decrypted: $decrypted');
+              },
+              child: Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                child: Text(
+                  "Upload File",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          fileUploaded
+              ? SizedBox(
+                  height: size.height * 2 / 100,
+                )
+              : Container(),
+          fileUploaded
+              ? Container(
+                  margin:
+                      EdgeInsets.symmetric(horizontal: size.width * 5 / 100),
+                  child: Text(
+                    'Here is your encoded output',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              : Container(),
+          fileUploaded
+              ? SizedBox(
+                  height: size.height * 1 / 100,
+                )
+              : Container(),
+          fileUploaded
+              ? Container(
+                  margin:
+                      EdgeInsets.symmetric(horizontal: size.width * 5 / 100),
+                  child: Text(
+                    encrypted.toString(),
+                    style: TextStyle(
+                      color: Colors.pink,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                )
+              : Container(),
+        ],
       ),
     );
   }
