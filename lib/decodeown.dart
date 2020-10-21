697a0e2a3ec1cf0337b86e566e1d748b6ec7a344
Siddharth Singh <siddharthsingh@Siddharths-MacBook-Pro.local>
2020-10-22 00:55:28 +0530

    third page done

--- lib/decodeown.dart
+++ lib/decodeown.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DecodeOwnScreen extends StatefulWidget {
   @override
@@ -8,6 +9,9 @@ class DecodeOwnScreen extends StatefulWidget {
 class _DecodeOwnScreenState extends State<DecodeOwnScreen> {
   @override
   List numbers = [];
+  String currentNumber = '';
+  TextEditingController number = TextEditingController();
+  bool isEmpty = true;
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
@@ -40,13 +44,127 @@ class _DecodeOwnScreenState extends State<DecodeOwnScreen> {
             height: size.height * 1 / 100,
           ),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: size.width * 5 / 100),
-            child: Text(
-              '',
-              style: TextStyle(
-                color: Colors.pink,
-                fontSize: 26,
-                fontWeight: FontWeight.w600,
+            height: size.height * 6 / 100,
+            width: double.maxFinite,
+            child: ListView.builder(
+              shrinkWrap: false,
+              scrollDirection: Axis.horizontal,
+              itemCount: numbers.length,
+              padding: EdgeInsets.symmetric(horizontal: size.width * 6 / 100),
+              itemBuilder: (context, index) {
+                return Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size.width * 2 / 100,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    color: Theme.of(context).accentColor,
+                  ),
+                  margin: EdgeInsets.only(
+                    right: size.width * 2 / 100,
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(
+                          numbers[index].toString(),
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          FontAwesomeIcons.times,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            numbers.removeAt(index);
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 2 / 100,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 6 / 100,
+              right: MediaQuery.of(context).size.width * 6 / 100,
+            ),
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 1 / 100,
+              right: MediaQuery.of(context).size.width * 1 / 100,
+              bottom: MediaQuery.of(context).size.height * 0.5 / 100,
+              top: MediaQuery.of(context).size.height * 0.1 / 100,
+            ),
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 6 / 100,
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).accentColor),
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+            ),
+            child: TextFormField(
+              controller: number,
+              onChanged: (value) {
+                currentNumber = value;
+              },
+              cursorColor: Theme.of(context).accentColor,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  FontAwesomeIcons.key,
+                  color: Theme.of(context).accentColor,
+                ),
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                hintText: "Enter a number",
+                hintStyle: TextStyle(
+                  fontSize: MediaQuery.of(context).size.height * 2 / 100,
+                  fontWeight: FontWeight.w400,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 3 / 100,
+          ),
+          Container(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  numbers.add(int.parse(currentNumber));
+                  number.clear();
+                  print(numbers);
+                });
+              },
+              splashColor: Theme.of(context).accentColor,
+              child: Center(
+                child: Text(
+                  "Add",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height * 2 / 100,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).accentColor,
+                  ),
+                ),
               ),
             ),
           ),
